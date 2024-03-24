73e23a7eb41525080a73fe88e219c0edc0e34724
alalidana <148446876+alalidana@users.noreply.github.com>
2024-03-24 12:48:45 +0000

    fix form class started storing the info in a txt file

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template
+from form import Surveyform
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'speak-up'
 @app.route('/')
 def index():
     return render_template('welcomepage.html')
@@ -9,9 +11,16 @@ def index():
 def info():
     return render_template('informationpage.html')
 
-@app.route('/questionnaire')
+@app.route('/questionnaire', methods=['GET', 'POST'])
 def questionnaire():
-    return render_template('datacollectionpage.html')
+    #initializing form
+    form= Surveyform()
+    #check if form is submit and valid
+    if form.validate_on_submit():
+        #opens the file in append mode and write form data
+        with open('form_responses.txt', 'a') as file:
+            file.write(f"Name: {form.name.data}\n")
+    return render_template('datacollectionpage.html', form= form)
 
 @app.route('/contact')
 def contact():
