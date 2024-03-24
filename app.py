f19326ec09431fc638d0c088433484a1183293c6
alalidana <148446876+alalidana@users.noreply.github.com>
2024-03-24 00:29:37 +0000

    deleting main.py and adding changes to app.py

--- app.py
+++ app.py
@@ -5,5 +5,17 @@ app = Flask(__name__)
 def index():
     return render_template('welcomepage.html')
 
+@app.route('/info')
+def info():
+    return render_template('informationpage.html')
+
+@app.route('/questionnaire')
+def questionnaire():
+    return render_template('datacollectionpage.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contactpage.html')
+
 if __name__ == '__main__':
    app.run(debug = True)
