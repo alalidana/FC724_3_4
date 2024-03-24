01b59c2ac98eb97abe9ca676d0ec2ee8c105c89d
alalidana <148446876+alalidana@users.noreply.github.com>
2024-03-24 14:01:52 +0000

    adding the categories to the form responses

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, flash
 from form import Surveyform
 
 app = Flask(__name__)
@@ -20,6 +20,20 @@ def questionnaire():
         #opens the file in append mode and write form data
         with open('form_responses.txt', 'a') as file:
             file.write(f"Name: {form.name.data}\n")
+            file.write(f"Student Number: {form.student_number.data}\n")
+            file.write(f"Email: {form.email.data}\n")
+            file.write(f"Attendance Rate: {form.attendance.data}\n")
+            file.write(f"Number of Students: {form.students_number.data}\n")
+            file.write(f"Self Learning Hours: {form.self_learning.data}\n")
+            file.write(f"Academic Performance: {form.performance.data}\n")
+            file.write(f"Challenging Courses: {form.challenging_courses.data}\n")
+            file.write(f"Teaching Methods Satisfaction: {form.teaching_methods.data}\n")
+            file.write(f"Resources Satisfaction: {form.resources_satisfaction.data}\n")
+            file.write(f"Improvement Suggestions: {form.improvement_suggestions.data}\n")
+            file.write(f"Goals: {form.goals.data}\n")
+            file.write("------------------------------------------------\n")
+        #flash system to display feedback to the user.
+        flash('Your survey response has been successfully submitted!', 'success')
     return render_template('datacollectionpage.html', form= form)
 
 @app.route('/contact')
