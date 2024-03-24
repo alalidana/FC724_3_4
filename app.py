f359fb85c0dc4fc31b9afa3311872d8b6c9a9bf9
alalidana <148446876+alalidana@users.noreply.github.com>
2024-03-24 11:26:14 +0000

    adding more questions to the class

--- app.py
+++ app.py
@@ -23,6 +23,12 @@ def contact():
 class Surveyform(FlaskForm):
     name = StringField('Name', Validator=[DataRequired])
     student_number = StringField('Student number', validators=[DataRequired(), Length(min=7, max=7)])
-
+    email = StringField('Email', Validator=[DataRequired, Email()])
+    attendance = StringField('Attendance rate', validators=[DataRequired()])
+    students = StringField('Total number of students in Class', validators=[DataRequired()])
+    self_learning = RadioField('Hours Spent on Self-Learning', choices=[('1-2', '1-2 hours'),
+                                                                        ('3-4', '3-4 hours'), ('5-6', '5-6 hours'), ('7+', '7+ hours')])
+    performance = SelectField('Academic Performance', choices=[('1', '1'), ('2', '2'), ('3', '3'), ('4', '4'),
+                                                               ('5', '5'), ('6', '6'), ('7', '7'), ('8', '8'), ('9', '9'), ('10', '10')])
 if __name__ == '__main__':
    app.run(debug = True)
